7b2da038df9792a1a1b6d940bf75157944814ee8
Prite <prite_mydream@hotmail.com>
2020-08-12 18:08:58 +0700

    Initial Commit

---
+++ RandomFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFood
+//
+//  Created by prite on 12/8/2563 BE.
+//  Copyright © 2563 prite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
